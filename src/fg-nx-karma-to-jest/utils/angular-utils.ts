2b94bdf18d808f58aa1e20c3bbeb094d16d6cdc7
Fabian Gosebrink <fabian.gosebrink@outlook.com>
2020-03-07 15:59:42 +0100

    adding angular utils

--- src/fg-nx-karma-to-jest/utils/angular-utils.ts
+++ src/fg-nx-karma-to-jest/utils/angular-utils.ts
@@ -1,6 +1,28 @@
 import { WorkspaceDefinition } from '@angular-devkit/core/src/workspace';
+import { experimental } from '@angular-devkit/core';
+import {
+  SchematicsException,
+  SchematicContext
+} from '@angular-devkit/schematics';
 
 export const ANGULAR_JSON_FILENAME = 'angular.json';
 export function getAngularWorkspaceObject(angularJsonFile: Buffer) {
   return JSON.parse(angularJsonFile.toString('utf-8')) as WorkspaceDefinition;
+}
+
+export function isRealProject(
+  project: experimental.workspace.WorkspaceProject,
+  _context: SchematicContext
+) {
+  if (!project?.architect) {
+    throw new SchematicsException(`no 'architect' section found`);
+  }
+
+  const testSection = project?.architect['test'];
+
+  if (!testSection) {
+    return false;
+  }
+
+  return true;
 }
