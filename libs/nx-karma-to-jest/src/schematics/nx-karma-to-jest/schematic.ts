be24990ff30d88710a6a2c0545b37ec6bd7e2530
Fabian Gosebrink <fabian.gosebrink@outlook.com>
2020-04-06 13:24:11 +0200

    adding more dependencies and small lintings

--- libs/nx-karma-to-jest/src/schematics/nx-karma-to-jest/schematic.ts
+++ libs/nx-karma-to-jest/src/schematics/nx-karma-to-jest/schematic.ts
@@ -2,7 +2,7 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import { runNxKarmaToJest } from './actions';
 import { NxKarmaToJestSchematicSchema } from './schema';
 
-export default function(options: NxKarmaToJestSchematicSchema): Rule {
+export default function (options: NxKarmaToJestSchematicSchema): Rule {
   return (host: Tree, context: SchematicContext) => {
     return runNxKarmaToJest(host, context);
   };
